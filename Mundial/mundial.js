491ee3662a858b377076bb414198d05f6eef1422
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:26:20 -0600

    Add files via upload

---
+++ Mundial/mundial.js
@@ -0,0 +1,18 @@
+let mundial = prompt("Digite un año");
+if (mundial == "2002") {
+    alert(" En ese año si hubo mundial")
+} else if (mundial == "2006") {
+    alert(" En ese año si hubo mundial")
+} else if (mundial == "2010") {
+    alert(" En ese año si hubo mundial")
+} else if (mundial == "2014") {
+    alert(" En ese año si hubo mundial")
+} else if (mundial == "2018") {
+    alert(" En ese año si hubo mundial")
+} else if (mundial == "2022") {
+    alert(" Por problemas con murcielagos se tuvo que hacer ese año")
+} else if (mundial== "2026") {
+    alert(" Esperemos que si haya mundial")
+} else {
+    alert("Ese año no hubo mundial")
+}
