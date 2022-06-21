3ddd06653b9e9952ca219f493d64fcf057a1e4a2
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:27:05 -0600

    Add files via upload

---
+++ Mundial pais/paisesm.js
@@ -0,0 +1,20 @@
+let paises = prompt("Digite un pais");
+if (paises == "Brasil") {
+    alert("Este pais ha ganado un mundial")
+} else if (paises == "Italia") {
+    alert("Este pais ha ganado un mundial")
+} else if (paises == "Alemania") {
+    alert("Este pais ha ganado un mundial")
+} else if (paises == "Francia") {
+    alert("Este pais ha ganado un mundial")
+} else if (paises == "Argentina") {
+    alert("Este pais ha ganado un mundial")
+} else if (paises == "Uruguay") {
+    alert("Este pais ha ganado un mundial")
+} else if (paises == "España") {
+    alert("Este pais ha ganado un mundial")
+} else if (paises == "Inglaterra") {
+    alert("Este pais ha ganado un mundial")
+} else {
+    alert("Este pais no ha ganado un mundial")
+}
