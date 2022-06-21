81beb3123477ed7e8a2cedc79744bf48e2edd43a
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:27:54 -0600

    Add files via upload

---
+++ Numeros par/numeros.js
@@ -0,0 +1,9 @@
+var numero
+numero = parseInt(prompt("Favor ingresar un numero"));
+if (numero % 2 == 0) {
+    alert("El numero es par")
+} else if (numero % 2 == 1) {
+    alert("El numero es impar")
+}
+
+    
