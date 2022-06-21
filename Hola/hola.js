9660840b67de0b606553b0d365dd3f66bef2e438
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:14:30 -0600

    Add files via upload

---
+++ Hola/hola.js
@@ -0,0 +1,16 @@
+let hola = prompt("Digite un hola como usted quiera");
+if (hola == "Hola") {
+    alert("hola,buen dia")
+} else if ("hola") {
+    alert("hola,buen dia")
+} else if ("HOLA") {
+    alert("hola,buen dia")
+} else if ("ola") {
+    alert("hola,buen dia")
+} else if ("hi") {
+    alert("hola,buen dia")
+} else if ("hello") {
+    alert("hola,buen dia")
+} else {
+    alert("No dogito un hola, porfavor digitar un hola")
+}
