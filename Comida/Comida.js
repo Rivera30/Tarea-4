22a573bd30a87815e11283d0f4522f85b2a0242b
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:25:46 -0600

    Add files via upload

---
+++ Comida/Comida.js
@@ -0,0 +1,8 @@
+let comida = prompt("Si usted tiene antojo de algo salado escoja la opción (1) pero si tiene antojo de algo dulce escoja (2)");
+if (comida == "1") {
+    alert(" Usted escogio pan de pizza y pan con jamón y queso")
+} else if (comida == "2") {
+    alert(" Usted escogio flauta de guayaba, cacho de dulce de leche y pañuelo de crema pastelera")
+} else {
+    alert("Usted no escogio ninguna de las opciones. Porfavor escoger una de las opciones")
+}
