7d172c2b3d1a5beda84b6e85fa7f385893eeedc4
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:14:03 -0600

    Add files via upload

---
+++ Paises/paises.js
@@ -0,0 +1,52 @@
+let paises = prompt("Digite un pais");
+if (paises == "Costa rica") {
+    alert( paises + " es un pais del cotinente de América")
+} else if (paises == "Argentina") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "Colombia") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "Nicaragua") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "Panamá") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "Uruguay") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "Estados Unidos") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "Canadá") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "México") {
+    alert(paises + " es un pais del cotinente de América")
+} else if (paises == "España") {
+    alert(paises + " es un pais del cotinente de Europa")
+} else if (paises == "Italia") {
+    alert(paises + " es un pais del cotinente de Europa")
+} else if (paises == "Francia") {
+    alert(paises + " es un pais del cotinente de Europa")
+} else if (paises == "Rumania") {
+    alert(paises + " es un pais del cotinente de Europa")
+} else if (paises == "Inglaterra") {
+    alert(paises + " es un pais del cotinente de Europa")
+} else if (paises == "Alemania") {
+    alert(paises + " es un pais del cotinente de Europa")
+} else if (paises == "Nigeria") {
+    alert(paises + " es un pais del cotinente de África")
+} else if (paises == "Kenia") {
+    alert(paises + " es un pais del cotinente de África")
+} else if (paises == "Ghana") {
+    alert(paises + "es un pais del cotinente de África")
+} else if (paises == "Marruecos") {
+    alert(paises +  "es un pais del cotinente de África")
+} else if (paises == "Senegal") {
+    alert(paises + " es un pais del cotinente de África")
+} else if (paises == "Nueva Zelanda") {
+    alert(paises + " es un pais del cotinente de Oceanía")
+} else if (paises == "Australia") {
+    alert(paises + " es un pais del cotinente de Oceanía")
+} else if (paises == "Fiyi") {
+    alert(paises + " es un pais del cotinente de Oceanía")
+} else if (paises == "Tonga") {
+    alert(paises + " es un pais del cotinente de Oceanía")
+} else {
+    alert("No introdujo un pais.Porfavor ingresar un pais")
+}
