210788e78aae831697667186ec3b59e8c04fb742
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:15:05 -0600

    Add files via upload

---
+++ Buenos dias/buenos.js
@@ -0,0 +1,10 @@
+let buenos = prompt("Digite un buenos dias, buenas tardes o un buenas noches");
+if (buenos == "buenos dias") {
+    alert(buenos + "")
+} else if (buenos == "buenas tardes") {
+    alert(buenos + "")
+} else if (buenos == "buenas noches") {
+    alert(buenos + "")
+} else {
+    alert("No digito ninguna de las opciones, porfavor digitar una de las siguientes opciones.")
+}
