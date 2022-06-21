80d323521c10fff489a2a5cc0449bb8e532f89ff
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:27:27 -0600

    Add files via upload

---
+++ Futból/futbol.js
@@ -0,0 +1,12 @@
+let futbol = prompt("Digite un jugador de futból");
+if (futbol == "Messi") {
+    alert("Es uno de los mejores jugadores del mundo")
+} else if (futbol == "Cristiano") {
+    alert("Es uno de los mejores jugadores del mundo")
+} else if (futbol == "Maradona") {
+    alert("Es uno de los mejores jugadores del mundo")
+} else if (futbol == "Keylor") {
+    alert("Es uno de los mejores jugadores del mundo")
+} else {
+    alert("Este jugador no es uno de los mejores del mundo")
+}
