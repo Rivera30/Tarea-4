a81239ba091cb93c2d4dd5394253232d19d6e552
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:13:17 -0600

    Add files via upload

---
+++ Color semaforo/semaforo.js
@@ -0,0 +1,10 @@
+let color = prompt("Digite el color del semaforo")
+if (color == "rojo") {
+    alert("Alto")
+} else if (color == "amarillo") {
+    alert("Baje la velocidad")
+} else if (color == "verde") {
+    alert("Puede seguir avanzando")
+} else {
+    alert("NO SE INGRESO LOS VALORES CORRECTO, POR FAVOR RECARGUE LA PAGINA Y ESCRIBA VERDE, AMARILLO, ROJO")
+}
