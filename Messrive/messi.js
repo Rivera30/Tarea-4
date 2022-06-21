a2e02bce7724ce853593dc1c5fdddd592eb55bab
Rivera30 <106450228+Rivera30@users.noreply.github.com>
2022-06-20 21:26:39 -0600

    Add files via upload

---
+++ Messrive/messi.js
@@ -0,0 +1,16 @@
+let messi = prompt("Digite un año");
+if (messi == "2010") {
+    alert(" En ese año Messi si gano el balón de oro")
+} else if (messi == "2011") {
+    alert(" En ese año Messi si gano el balón de oro")
+} else if (messi == "2012") {
+    alert(" En ese año Messi si gano el balón de oro")
+} else if (messi == "2015") {
+    alert(" En ese año Messi si gano el balón de oro")
+} else if (messi == "2019") {
+    alert(" En ese año Messi si gano el balón de oro")
+} else if (messi == "2021") {
+    alert(" En ese año Messi si gano el balón de oro")
+} else {
+    alert("Ese año no gano balón de oro Messi")
+}
